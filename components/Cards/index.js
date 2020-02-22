e39690bd7e16e49dff7939a52b78e564f4f3bfbe
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-02-21 18:02:26 -0800

    created article component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+// function articleComponent() {
+//     const card = document.createElement('div');
+//     card.classList.add('card');
+
+//     const headline = document.createElement('div');
+//     headline.classList.add('headline');
+//     headline.textContent = `${}`;
+//     card.appendChild(headline);
+
+//     const author = document.createElement('div');
+//     author.classList.add('author');
+//     card.appendChild(author);
+
+//     const imgContainer = document.createElement('div');
+//     imgContainer.classList.add('img-container');
+//     author.appendChild(imgContainer);
+
+//     const newImage = document.createElement('img');
+//     newImage.src =
+//     imgContainer.appendChild(newImage);
+
+//     const authorsName = document.createElement('span');
+//     authorsName.textContent = `By ${}`;
+//     author.appendChild(authorsName);
+
+//     return card;
+// }
+
+// const cardsContainer = document.querySelector('.cards-container');
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
